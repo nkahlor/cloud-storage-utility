8bd50d8ae2e56bd620ee9349d5c4c41839a6cdd5
Nicholas Kahlor <nicholas.kahlor@aa.com>
2021-03-02 16:23:40 -0700

    scrub a dub dub

--- build/lib/cloud_storage_utility/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-## Welcome to the API documentation!
-
-For most applications, the `file_broker` module will be all you need.
-
-If you want to add a new platform implementation, checkout `common/base_cloud_storage` and `platforms/`.
-"""
--- build/lib/cloud_storage_utility/common/cloud_local_map.py
+++
@@ -1,5 +0,0 @@
-class CloudLocalMap:
-    """ Maps a local filepath to a remote cloud key """
-    def __init__(self, cloud_key, local_filepath):
-        self.cloud_key = cloud_key
-        self.local_filepath = local_filepath
