821c3f1c378f0b486ffd81503ead3ff7fbcf1211
nkahlor <nkahlor@gmail.com>
2021-04-23 08:46:33 -0700

    fix up unit tests

--- tests/test_ibm_cloud_storage.py
+++ tests/test_ibm_cloud_storage.py
@@ -1,6 +1,7 @@
-import logging
-
+from cloud_storage_utility.config import config
+import aiohttp
 import pytest
+from aioresponses import aioresponses
 
 from cloud_storage_utility.platforms.ibm_cloud_storage import IbmCloudStorage
 
@@ -8,100 +9,96 @@ from cloud_storage_utility.platforms.ibm_cloud_storage import IbmCloudStorage
 class TestIbmCloudStorage:
     TEST_BUCKET = "test-bucket"
     TEST_FILE_PATH = "test_data/test_data_1.txt"
-    NONEXISTENT_FILE = "test_data/doesnt_exits.txt"
+    NONEXISTENT_FILE = "test_data/nonexistent.txt"
 
     @pytest.mark.asyncio
     async def test_upload_file_succeeds(self, mocker):
-        ibm_service = IbmCloudStorage()
+        async with aiohttp.ClientSession() as session:
+            ibm_service = IbmCloudStorage(session)
 
-        mock_file = mocker.mock_open(read_data="test")
-        mocker.patch(
-            "cloud_storage_utility.platforms.ibm_cloud_storage.open", mock_file
-        )
+            mock_file = mocker.mock_open(read_data="test")
+            mocker.patch(
+                "cloud_storage_utility.platforms.ibm_cloud_storage.open", mock_file
+            )
 
-        mock_upload_fileobj = mocker.Mock()
-        ibm_service._IbmCloudStorage__cos.Object = mocker.Mock(  # type: ignore
-            return_value=mock_upload_fileobj
-        )
+            with aioresponses() as mock_response:
+                endpoint = f"{config.IBM_CONFIG['cos_endpoint']}/test/test_data_1.txt"
+                mock_response.put(endpoint, status=200)
+                mock_response.post(
+                    f"{config.IBM_CONFIG['auth_endpoint']}",
+                    payload={"access_token": "", "expiration": ""},
+                )
+                response = await ibm_service.upload_file(
+                    "test", "test_data_1.txt", self.TEST_FILE_PATH
+                )
 
-        res = await ibm_service.upload_file(
-            self.TEST_BUCKET, "test", self.TEST_FILE_PATH, None
-        )
-
-        mock_upload_fileobj.upload_fileobj.assert_called()
-        assert res is True
+            assert response is True
 
     @pytest.mark.asyncio
     async def test_upload_file_executes_its_callback_on_success(self, mocker):
+        async with aiohttp.ClientSession() as session:
+            ibm_service = IbmCloudStorage(session)
+            mock_callback = mocker.Mock()
 
-        service = IbmCloudStorage()
+            mock_file = mocker.mock_open(read_data="test")
+            mocker.patch(
+                "cloud_storage_utility.platforms.ibm_cloud_storage.open", mock_file
+            )
 
-        mock_callback = mocker.Mock()
-        mock_file = mocker.mock_open(read_data="test")
-        mocker.patch(
-            "cloud_storage_utility.platforms.ibm_cloud_storage.open", mock_file
-        )
-        mock_upload_fileobj = mocker.Mock()
-        service._IbmCloudStorage__cos.Object = mocker.Mock(  # type: ignore
-            return_value=mock_upload_fileobj
-        )
+            with aioresponses() as mock_response:
+                endpoint = f"{config.IBM_CONFIG['cos_endpoint']}/test/test_data_1.txt"
+                mock_response.put(endpoint, status=200)
+                mock_response.post(
+                    f"{config.IBM_CONFIG['auth_endpoint']}",
+                    payload={"access_token": "", "expiration": ""},
+                )
+                response = await ibm_service.upload_file(
+                    "test",
+                    "test_data_1.txt",
+                    self.TEST_FILE_PATH,
+                    callback=mock_callback,
+                )
 
-        await service.upload_file(
-            self.TEST_BUCKET, "test", self.TEST_FILE_PATH, mock_callback
-        )
-
-        mock_callback.assert_called()
+            assert response is True
+            mock_callback.assert_called()
 
     @pytest.mark.asyncio
     async def test_upload_file_executes_callback_on_fail(self, mocker):
-        # Forcing it to throw a file not found error
+        async with aiohttp.ClientSession() as session:
+            ibm_service = IbmCloudStorage(session)
+            mock_callback = mocker.Mock()
 
-        service = IbmCloudStorage()
+            with aioresponses() as mock_response:
+                endpoint = f"{config.IBM_CONFIG['cos_endpoint']}/test/test_data_1.txt"
+                mock_response.put(endpoint, status=404)
+                mock_response.post(
+                    f"{config.IBM_CONFIG['auth_endpoint']}",
+                    payload={"access_token": "", "expiration": ""},
+                )
+                response = await ibm_service.upload_file(
+                    "test",
+                    "test_data_1.txt",
+                    self.TEST_FILE_PATH,
+                    callback=mock_callback,
+                )
 
-        mock_callback = mocker.Mock()
-        mock_upload_file = mocker.Mock()
-        service._IbmCloudStorage__cos.Object = mocker.Mock(  # type: ignore
-            return_value=mock_upload_file
-        )
-
-        await service.upload_file(
-            self.TEST_BUCKET, "test", self.NONEXISTENT_FILE, mock_callback
-        )
-
-        mock_callback.assert_called()
-
-    @pytest.mark.asyncio
-    async def test_upload_file_skips_callback_when_not_defined(self, mocker):
-        service = IbmCloudStorage()
-
-        mock_callback = mocker.Mock()
-        mock_file = mocker.mock_open(read_data="test")
-        mocker.patch(
-            "cloud_storage_utility.platforms.ibm_cloud_storage.open", mock_file
-        )
-        mock_upload_fileobj = mocker.Mock()
-        service._IbmCloudStorage__cos.Object = mocker.Mock(  # type: ignore
-            return_value=mock_upload_fileobj
-        )
-
-        await service.upload_file(self.TEST_BUCKET, "test", self.NONEXISTENT_FILE, None)
-
-        mock_callback.assert_not_called()
+            assert response is False
+            mock_callback.assert_called()
 
     @pytest.mark.asyncio
     async def test_upload_file_fails_on_missing_local_file(self, mocker):
+        async with aiohttp.ClientSession() as session:
+            ibm_service = IbmCloudStorage(session)
 
-        service = IbmCloudStorage()
+            with aioresponses() as mock_response:
+                endpoint = f"{config.IBM_CONFIG['cos_endpoint']}/test/test_data_1.txt"
+                mock_response.put(endpoint, status=200)
+                mock_response.post(
+                    f"{config.IBM_CONFIG['auth_endpoint']}",
+                    payload={"access_token": "", "expiration": ""},
+                )
+                response = await ibm_service.upload_file(
+                    "test", "test_data_1.txt", self.TEST_FILE_PATH
+                )
 
-        mock_logger = mocker.patch.object(logging, "error")
-        mock_upload_fileobj = mocker.Mock()
-        service._IbmCloudStorage__cos.Object = mocker.Mock(  # type: ignore
-            return_value=mock_upload_fileobj
-        )
-
-        request_status = await service.upload_file(
-            self.TEST_BUCKET, "test", self.NONEXISTENT_FILE
-        )
-
-        assert request_status is False
-        mock_logger.assert_called()
+            assert response is False
